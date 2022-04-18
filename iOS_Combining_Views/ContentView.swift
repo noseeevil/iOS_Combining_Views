1479789dc27319561e15f648f61c0e851e994738
noseeevil <oleksandrpatsora@gmail.com>
2022-04-18 14:29:26 +0300

    compose the Detail View

--- iOS_Combining_Views/ContentView.swift
+++ iOS_Combining_Views/ContentView.swift
@@ -9,8 +9,41 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        
+        VStack {
+            
+            MapView()
+                .frame(height: 300)
+                .ignoresSafeArea(edges: .top)
+            
+            CircleImage()
+                .offset(y: -130)
+                .padding(.bottom, -130)
+            
+            VStack(alignment: .leading) {
+                Text("Home")
+                    .font(.title)
+                    .fontWeight(.bold)
+                    .foregroundColor(/*@START_MENU_TOKEN@*/.green/*@END_MENU_TOKEN@*/)
+                HStack {
+                    Text("Kyiv oblast")
+                        .font(.subheadline)
+                    Spacer()
+                    Text("Ukraine")
+                        .font(.subheadline)
+                }
+                .font(.subheadline)
+                .foregroundColor(.secondary)
+                
+                Divider()
+                
+                Text("Best place").font(.title2)
+                Text("in your life")
+                
+            }.padding()
+            Spacer()
+        }
+               
     }
 }
 
